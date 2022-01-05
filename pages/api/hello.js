8428871b8edf73b034178422c048e8c22292013c
ioana suciu <suciuioanamagdalena@gmail.com>
2022-01-05 10:57:16 +0200

    Add

---
+++ pages/api/hello.js
@@ -0,0 +1,4 @@
+// JavaScript source code
+export default function handler(req, res) {
+    res.status(200).json({ text: 'Hello' })
+}
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+    purge: [
+        './pages/**/*.js',
+        './components/**/*.js'
+    ]
+}
